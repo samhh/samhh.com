3a8d3810ae368e30f79536e29b4e39e45cc3ee15
Sam A. Horvath-Hunt <hello@samhh.com>
2020-09-20 22:34:27 +0100

    Add NonEmptyArray alias

--- src/aliases.d.ts
+++ src/aliases.d.ts
@@ -1,4 +1,4 @@
 type Option<A> = import('fp-ts/lib/Option').Option<A>;
-
+type NonEmptyArray<A> = import('fp-ts/lib/NonEmptyArray').NonEmptyArray<A>;
 type Predicate<A> = import('fp-ts/lib/function').Predicate<A>;
 
